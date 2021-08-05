1ba9a6a52947f29ca21912b2c1ea680adca32018
Olesya Irkhina <itekkie888@gmail.com>
2021-08-05 13:08:33 -0400

    update schema.sql and query

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -29,3 +29,42 @@ INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--new table with +1 colomn
+DROP TABLE retirement_info;
+
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Joining departments and dept_manager tables
+SELECT departments.dept_name,
+     dept_manager.emp_no,
+     dept_manager.from_date,
+     dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+retirement_info.last_name,
+    dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+-- add PK to retiring employees
+ALTER TABLE retirement_info
+ADD PRIMARY KEY (emp_no);
+
+-- add FK to retiring employees
+
+ALTER TABLE retirement_info
+ADD FOREIGN KEY (emp_no) REFERENCES employees(emp_no);
+
+-- Check the table
+SELECT * FROM retirement_info;
