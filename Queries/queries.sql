f7806b4dff364b5187ebd30afae5430d9c1ecf8e
Olesya Irkhina <itekkie888@gmail.com>
2021-08-05 15:00:41 -0400

    add emp_info.csv

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -103,3 +103,42 @@ LEFT JOIN dept_employees as de
 ON ce.emp_no = de.emp_no
 GROUP BY de.dept_no
 ORDER BY de.dept_no;
+
+--order by
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+--new epm_info filtered table
+SELECT emp_no,
+    first_name,
+    last_name,
+    gender
+INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--join INNER to the salaries table to add the to_date and Salary columns to our query.
+
+--List 1: Employee Information
+SELECT e.emp_no,
+    e.first_name,
+	e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
+--comment out the INTO line so that we don't run it with the res
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+--to_date
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+    AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+    --filter we need is the to_date of 999-01-01 from the dept_emp
+    AND (de.to_date = '9999-01-01');
+
+
+
