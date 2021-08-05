a7ee767a5410610cde0d4bd0beed438dca8d47bd
Olesya Irkhina <itekkie888@gmail.com>
2021-08-05 07:17:19 -0400

    update schema.sql

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -36,23 +36,30 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
+--DROP TABLE dept_manager CASCADE;
+
 CREATE TABLE dept_employees (
-  dept_no VARCHAR(4) NOT NULL,
-  emp_no INT NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (dept_no, emp_no)
+	  emp_no INT NOT NULL,
+	  dept_no VARCHAR NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
 );
+
+--DROP TABLE titles CASCADE;
+
+DROP TABLE titles CASCADE;
 
 CREATE TABLE titles (
-  title VARCHAR NOT NULL,
   emp_no INT NOT NULL,
+  title VARCHAR NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  PRIMARY KEY (emp_no, title, from_date)
 );
 
-SELECT * FROM departments
+
+--SELECT * FROM departments
