22f8ef5407e4c0a5bdcc575f1741869eebd7d710
Olesya Irkhina <itekkie888@gmail.com>
2021-08-05 15:10:43 -0400

    update queries and add .csvs files

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -140,5 +140,29 @@ WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
     --filter we need is the to_date of 999-01-01 from the dept_emp
     AND (de.to_date = '9999-01-01');
 
+-- List 2 of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+-- INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
 
-
+-- List 3: Department Retirees
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_employees AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
